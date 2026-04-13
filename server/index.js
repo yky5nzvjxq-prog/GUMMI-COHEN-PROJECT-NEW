e7debe5590a1342c6dd35ac0177d5787b3f1143f
Noam Cohen <noam47755@icloud.com>
2026-04-13 14:04:22 +0300

    fix port for render

--- server/index.js
+++ server/index.js
@@ -592,5 +592,5 @@ app.use((err, req, res, next) => {
   next();
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
